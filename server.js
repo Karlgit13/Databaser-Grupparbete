50f73cd9a1e128b86365183f66cc8b8daec0c1d9
Karlgit13 <karl.vareskog@gmail.com>
2025-07-21 11:57:31 +0200

    Folders & files, Server.js, DB.js & dependencies installed.

---
+++ server.js
@@ -0,0 +1,24 @@
+import express from "express"
+import dotenv from "dotenv"
+import userRoutes from "./routes/userRoutes.js"
+import channelRoutes from "./routes/channelRoutes.js"
+import messageRoutes from "./routes/messageRoutes.js"
+import subscriptionRoutes from "./routes/subscriptionRoutes.js"
+
+dotenv.config()
+const app = express()
+app.use(express.json())
+
+
+//routes
+app.use("users", userRoutes)
+app.use("channels", channelRoutes)
+app.use("messages", messageRoutes)
+app.use("subscriptions", subscriptionRoutes)
+
+
+// server
+const PORT = process.env.PORT || 5000
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`)
+})
