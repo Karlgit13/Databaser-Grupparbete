f0cab7593001ae1f2ac4fbeb4c78b6a0389c29e9
Karlgit13 <karl.vareskog@gmail.com>
2025-07-23 11:47:22 +0200

    routes fixes

--- routes/channelRoutes.js
+++ routes/channelRoutes.js
@@ -3,15 +3,33 @@ import { pool } from "../db.js";
 
 const router = express.Router()
 
+
+// Get all channels
 router.get("/", async (req, res) => {
     try {
-        const response = await pool.query("SELECT * FROM channels");
-        res.json(response.rows);
+        const result = await pool.query("SELECT * FROM channels");
+        res.json(result.rows);
     } catch (error) {
         console.error("Error fetching users:", error);
         res.status(500).json({ error: "Internal Server Error" });
     }
 
 })
+
+
+// Create a new channel
+router.post("/", async (req, res) => {
+    const { name, owner_id } = req.body;
+    try {
+        const result = await pool.query(
+            `INSERT INTO channels (name, owner_id) VALUES ($1, $2) RETURNING *`,
+            [name, owner_id]
+        );
+        res.status(201).json(result.rows[0]);
+    } catch (error) {
+        console.error("Error creating channel:", error);
+        res.status(500).json({ error: "Internal Server Error" });
+    }
+}); s
 
 export default router;
--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -4,14 +4,31 @@ import { getAllUsers } from "../models/userModels.js";
 
 const router = express.Router();
 
+
+// Get all users
 router.get("/", async (req, res) => {
     try {
-        const response = await pool.query("SELECT * FROM users")
-        res.json(response.rows)
+        const users = await getAllUsers();
+        res.json(users)
     } catch (error) {
         console.error("error fetching users:", error);
         res.status(500).json({ error: "Internal Server Error" });
     }
 });
+
+
+
+// Create a new user
+router.post("/", async (req, res) => {
+    const { name, email } = req.body
+    try {
+        const newUser = await createUser(name, email)
+        res.status(201).json(newUser);
+    } catch (error) {
+        console.error("Error creating user:", error);
+        res.status(500).json({ error: "Internal Server Error" });
+    }
+});
+
 
 export default router;
